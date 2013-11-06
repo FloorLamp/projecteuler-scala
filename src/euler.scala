0c3189563314b9c0135af271b18d04bede0ba65c
Norton Wang <nortantivirus@gmail.com>
2013-11-06 17:20:25 -0500

    oh god those vars

--- src/euler.scala
+++ src/euler.scala
@@ -1,24 +1,20 @@
-package euler
-
-class euler {
-    def p6 = {
-        var sum = 0;
-        var sum_squared = 0;
-        for (i <- 1 to 100) {
-            sum += i;
-            sum_squared += math.pow(i, 2).toInt;
-        }
-        println(sum*sum - sum_squared);
+object euler {
+    def main(args: Array[String]) = {
+        p5
+        p6
     }
     
     def p5 = {
-        var prod = 1
-        for (i <- 1 to 20) {
-            var old = prod
-            while (prod % i != 0) {
-                prod += old
-                println(prod)
-            }
-        }
+        def r(max: Int, curr: Int = 1, temp: Int = 1, acc: Int = 1): Int = 
+            if (acc % curr == 0 && curr < max) r(max, curr + 1, acc, acc)
+            else if (acc % curr != 0) r(max, curr, temp, acc + temp)
+            else acc
+        val result = r(20)
+        println("p5", result)
+    }
+
+    def p6 = {
+        val result = math.pow((1 to 100).sum, 2) - (1 to 100).map( i => i*i ).sum
+        println("p6", result)
     }
 }
